fd04735a4a19ecabefd993dd150f6ad806d09061
HughParsonage <hugh.parsonage@gmail.com>
2017-08-31 22:54:02 +1000

    Add char key using Census decoder

---
+++ R/data-SA16_decoder.R
@@ -0,0 +1,4 @@
+#' SA16 decoder
+#' @description Decode between Census Codes and Census Names.
+
+"SA16_decoder"
---
+++ data-raw/put-SA16_decoder.R
@@ -0,0 +1,10 @@
+library(data.table)
+library(readxl)
+
+SA16_decoder <-
+  read_excel("./data-raw/data-packs/Metadata/2016Census_geog_desc_1st_release.xlsx",
+             sheet = 1) %>%
+  as.data.table %>%
+  .[ASGS_Structure != "SA1", .(ASGS_Structure, Census_Code_2016 = as.integer(Census_Code_2016), Census_Name_2016)]
+
+devtools::use_data(SA16_decoder, overwrite = TRUE)
