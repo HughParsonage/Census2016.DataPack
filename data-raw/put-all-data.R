9f95339dfc2b378b84c11d4923d106db1d748c34
HughParsonage <hugh.parsonage@gmail.com>
2018-01-02 02:17:06 +1100

    QC'd

--- data-raw/put-all-data.R
+++ data-raw/put-all-data.R
@@ -1,4 +1,8 @@
-for (Region in c("SA2", "SA3", "SA4", "LGA", "CED", "SED", "POA", "STE")) {
+for (Region in c("STE", "SED",
+                 # "SA1",
+                 "SA2", "SA3", "SA4", "LGA", "CED",
+                 "SSC",
+                 "POA")) {
   table_killer <- NULL; cat("\n-------", Region, "-------\n");
   invisible(source(knitr::purl("data-raw/put-data.Rmd", output = tempfile(), quiet = TRUE)))
 }
