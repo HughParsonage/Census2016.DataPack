26697a7babcc4c2d690119a45b08541139404355
HughParsonage <hugh.parsonage@gmail.com>
2018-01-02 22:37:43 +1100

    Update spec

--- data-raw/put-all-data.R
+++ data-raw/put-all-data.R
@@ -1,8 +1,9 @@
 for (Region in c("STE", "SED",
-                 # "SA1",
+                 "SA1",
                  "SA2", "SA3", "SA4", "LGA", "CED",
                  "SSC",
                  "POA")) {
+  in_for_loop <- TRUE
   table_killer <- NULL; cat("\n-------", Region, "-------\n");
   invisible(source(knitr::purl("data-raw/put-data.Rmd", output = tempfile(), quiet = TRUE)))
 }
